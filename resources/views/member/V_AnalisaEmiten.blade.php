cb78cd3d4fac3aed8ab6fda552598337ea2d86c0
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-11-23 14:44:14 +0700

    member - analisa emiten

--- resources/views/member/V_AnalisaEmiten.blade.php
+++ resources/views/member/V_AnalisaEmiten.blade.php
@@ -6,43 +6,29 @@
 @section('content')
   <div class="page-section">
   <div class="container bg-white">
-					<div class="col col-md-12 pt-3">
-						<div class="row ">
-							<card class="col col-md-6">
-								<h4>Analisis emiten perusahaan BRI:</h4>
-										Health<span class="pull-right strong"></span>
-										 <div class="progress">
-											<div class="progress-bar bg-info" role="progressbar" aria-valuenow="15"aria-valuemin="0" aria-valuemax="100" style="width:15%">1.5</div>
-										</div>
-										Growth<span class="pull-right strong"></span>
-										 <div class="progress">
-											<div class="progress-bar bg-success" role="progressbar" aria-valuenow="100"aria-valuemin="0" aria-valuemax="100" style="width:80%">8</div>
-										</div>
-									
-										Value<span class="pull-right strong"></span>
-										 <div class="progress">
-											<div class="progress-bar bg-warning" role="progressbar" aria-valuenow="8"aria-valuemin="0" aria-valuemax="100" style="width:12%">1.2</div>
-										</div>
-              </card>
-              <card class="col col-md-6 mb-3">
-								<h4>Analisis emiten perusahaan BNI:</h4>
-										Health<span class="pull-right strong"></span>
-										 <div class="progress">
-											<div class="progress-bar bg-info" role="progressbar" aria-valuenow="15"aria-valuemin="0" aria-valuemax="100" style="width:15%">1.5</div>
-										</div>
-										Growth<span class="pull-right strong"></span>
-										 <div class="progress">
-											<div class="progress-bar bg-success" role="progressbar" aria-valuenow="100"aria-valuemin="0" aria-valuemax="100" style="width:80%">8</div>
-										</div>
-									
-										Value<span class="pull-right strong"></span>
-										 <div class="progress">
-											<div class="progress-bar bg-warning" role="progressbar" aria-valuenow="8"aria-valuemin="0" aria-valuemax="100" style="width:12%">1.2</div>
-										</div>
-              </card>
-						</div>
+	<div class="col col-md-12 p-3">
+		<div class="row">
+			@foreach($data as $p)
+			<card class="col col-md-6">
+				<h4>Analisis emiten {{$p->kode_saham}}:</h4>
+					Health<span class="pull-right strong"></span>
+						<div class="progress">
+						<div class="progress-bar bg-info" role="progressbar" aria-valuenow="{{$p->health}}"aria-valuemin="0" aria-valuemax="100" style="width:{{$p->health}}%">{{$p->health / 10}}</div>
 					</div>
-        </div>
+					Growth<span class="pull-right strong"></span>
+						<div class="progress">
+						<div class="progress-bar bg-success" role="progressbar" aria-valuenow="{{$p->growth}}"aria-valuemin="0" aria-valuemax="100" style="width:{{$p->growth}}%">{{$p->growth / 10}}</div>
+					</div>
+				
+					Value<span class="pull-right strong"></span>
+						<div class="progress">
+						<div class="progress-bar bg-warning" role="progressbar" aria-valuenow="{{$p->value}}"aria-valuemin="0" aria-valuemax="100" style="width:{{$p->value}}%">{{$p->value / 10}}</div>
+					</div>
+			</card>
+			@endforeach
+			</div>
+		</div>
+	</div>
 
   </div>
 
