43988b33d17a234067e1485d48902b13395f742f
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-12-07 16:07:14 +0700

    view kalkulator finansial

---
+++ resources/views/member/V_KalkulatorFinansial.php
@@ -0,0 +1,37 @@
+@extends('layouts.temp')
+
+@section('judul1', 'Analisis Emiten Perusahaan')
+@section('judul', 'Analisis Emiten')
+
+@section('content')
+  <div class="page-section">
+  <div class="container ">
+	<div class="col col-md-12 p-3">
+		<div class="row">
+			@foreach($data as $p)
+			<card class="col col-md-6">
+				<div class="shadow p-3 mb-5 bg-white rounded">
+					<h4>Analisis emiten {{$p->kode_saham}}:</h4>
+					Health<span class="pull-right strong"></span>
+					<div class="progress">
+						<div class="progress-bar bg-info" role="progressbar" aria-valuenow="{{$p->health}}"aria-valuemin="0" aria-valuemax="100" style="width:{{$p->health}}%">{{$p->health / 10}}</div>
+					</div>
+					Growth<span class="pull-right strong"></span>
+					<div class="progress">
+						<div class="progress-bar bg-success" role="progressbar" aria-valuenow="{{$p->growth}}"aria-valuemin="0" aria-valuemax="100" style="width:{{$p->growth}}%">{{$p->growth / 10}}</div>
+					</div>
+					
+					Value<span class="pull-right strong"></span>
+					<div class="progress">
+						<div class="progress-bar bg-warning" role="progressbar" aria-valuenow="{{$p->value}}"aria-valuemin="0" aria-valuemax="100" style="width:{{$p->value}}%">{{$p->value / 10}}</div>
+					</div>
+				</div>
+			</card>
+			@endforeach
+			</div>
+		</div>
+	</div>
+
+  </div>
+
+@endsection
