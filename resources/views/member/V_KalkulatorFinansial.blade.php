fd041f8e0cb98b5cfa095647db6ad9beb19787bd
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-12-09 09:54:46 +0700

    perbaikan view kalkulator  fnansial

--- resources/views/member/V_KalkulatorFinansial.blade.php
+++ resources/views/member/V_KalkulatorFinansial.blade.php
@@ -25,17 +25,20 @@
 					<h3 class="title">Kalkulator Finansial</h3>
 					<div class="row">
 					  <div class="form-group col-sm-12">
-						<input id="nominal" type="number" class="form-control mb-2" name="nominal (Rupiah)" placeholder="Nominal" required aria-label="Card Holder" aria-describedby="basic-addon1" value="{{$nominal}}">
-						<input id="return" type="number" class="form-control  mb-2" name="return (%)" placeholder="Return" required aria-label="Card Holder" aria-describedby="basic-addon1" value="{{$return}}">
-						<input id="waktu" type="number" class="form-control" name="waktu (Tahun)" placeholder="Jangka Waktu" required aria-label="Card Holder" aria-describedby="basic-addon1" value="{{$waktu}}">
+						<input id="nominal" type="number" class="form-control mb-2" name="nominal" placeholder="Nominal (Rupiah)" required aria-label="Card Holder" aria-describedby="basic-addon1" value="{{$nominal}}">
+						<input id="return" type="number" class="form-control  mb-2" name="return" placeholder="Return (%)" required aria-label="Card Holder" aria-describedby="basic-addon1" value="{{$return}}">
+						<input id="waktu" type="number" class="form-control mb-2" name="waktu" placeholder="Jangka Waktu (Tahun)" required aria-label="Card Holder" aria-describedby="basic-addon1" value="{{$waktu}}">
+						@if($hasil != '')
+						<p>Perkiraan hasil investasi = {{$hasil}}</p>
+					  @endif
 					  </div>
+					  
 					  <div class="form-group col-sm-12">
 						<button type="submit" class="btn btn-primary btn-block" >Hitung</button>
+						<!-- <button type="reset" class="btn btn-primary btn-block" >Reset</button> -->
 					  </div>
 					</div>
-					@if($hasil != '')
-					<p>Perkiraan hasil investasi = {{$hasil}}</p>
-					@endif
+					
 				  </div>
 				</form>				
 				
